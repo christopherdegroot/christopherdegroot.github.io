64ad187e77dcb70b222b6dea738d2c4991bd4e89
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 15:10:14 -0700

    add parsedTypes to assign badge color based on actual hex codes used by pokemon for different types and render color to badge accordingly

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -11,6 +11,16 @@ const parsedStats = props.data.stats.map((stat)=>{
   return <p>{stat.stat.name}, {stat.base_stat}</p>
 })
 
+const parsedTypes = props.data.types.map((type)=>{
+  let typeColors = {"normal": "#a7a778", "fire": "#f08030", "water": "#6890f0", "grass": "#78c850", "electric": "#f8d030", "ice": "#98d8d8", "fighting": "#872e2d", "poison": "#a040a0", "ground": "#e0c068", "flying": "#a890f0", "psychic": "#f75887", "bug": "#a8b820", "rock": "#b8a038", "ghost": "#705898", "dark": "#705848", "dragon": "#7038f8", "steel": "#b8b8d0",  "fairy": "#f0b6bc"}
+  let badgeColor = '';
+  for (const typeColor in typeColors) {
+    if (type.type.name === typeColor)
+    badgeColor = typeColors[type.type.name]
+  }
+ return <Badge key={type.type.name} mx="2px" borderRadius="full" px="2" bgColor={badgeColor}>{type.type.name}</Badge>
+})
+
   return (
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
         <Image src={props.data.sprites.front_default} alt={props.data.sprites.front_default} />
