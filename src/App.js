c1fcf2ee619d3e86ef9b531e0c651211ad5f5ac1
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 13:27:01 -0700

    add useState for data and loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  Image,
+  Badge,
 } from '@chakra-ui/react';
 import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+
 
   const getData = function(search) {
     axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
@@ -26,8 +24,8 @@ useEffect(()=>{
 
 
   return (
-   <>
-   </>
+    <>
+    </>
   );
 }
 
