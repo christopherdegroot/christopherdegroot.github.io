b4ae225618fb7d9b6c2ef0ad6ad2627ebd3bcddc
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 15:59:22 -0700

    use prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Text, HStack, Input, Button, VStack, Box, Image, Badge } from '@chakra-ui/react';
+import {
+  Text,
+  HStack,
+  Input,
+  Button,
+  VStack,
+  Box,
+  Image,
+  Badge,
+} from '@chakra-ui/react';
 import axios from 'axios';
 import PokeCard from './components/PokeCard';
 
@@ -9,44 +18,55 @@ function App() {
   const [searchValue, setSearchValue] = useState('');
   const [error, setError] = useState(false);
 
-  const getData = function(search) {
+  const getData = function (search) {
     setLoading(true);
-    const randomPokemonId = Math.floor((Math.random() * 898) + 1);
-    
+    const randomPokemonId = Math.floor(Math.random() * 898 + 1);
+
     if (search) {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`).then(res => {
-      console.log(res.data);
-      setData(res.data);
-      setLoading(false);
-    })
-    .catch(function (error) {
-      console.log('logging error', error)
-      setLoading(false);
-      setError(true);
-    })
-  }
-    else
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`).then(res => {
-      console.log(res.data);
-      setData(res.data);
-      setLoading(false);
-    })
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+        .then(res => {
+          console.log(res.data);
+          setData(res.data);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          console.log('logging error', error);
+          setLoading(false);
+          setError(true);
+        });
+    } else
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
+        .then(res => {
+          console.log(res.data);
+          setData(res.data);
+          setLoading(false);
+        });
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-
   if (loading) return <p>Loading...</p>;
   if (!data) return <p>No data</p>;
-  if(error) return <p>Not found in Pokedex, please refresh and try again</p>
+  if (error) return <p>Not found in Pokedex, please refresh and try again</p>;
 
   return (
-    <VStack pt='2em'>
-      <HStack w='md'>
-       <Input onChange={e=>setSearchValue(e.target.value)} placeholder='Search Pokedex'></Input>
-        <Button onClick={()=>{getData(searchValue)}}>Search</Button>
+    <VStack pt="2em">
+      <HStack w="md">
+        <Input
+          onChange={e => setSearchValue(e.target.value)}
+          placeholder="Search Pokedex"
+        ></Input>
+        <Button
+          onClick={() => {
+            getData(searchValue);
+          }}
+        >
+          Search
+        </Button>
       </HStack>
       <PokeCard data={data}></PokeCard>
     </VStack>
