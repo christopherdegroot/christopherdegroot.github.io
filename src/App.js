a5ae7334c9f699e45189cce3bbea107480cde662
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 13:09:48 -0700

    move chakra ui wrapper to index.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ import {
 
 function App() {
   return (
-    <ChakraProvider>
-  
-    </ChakraProvider>
+   <>
+   </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
+  <ChakraProvider>
     <App />
+  </ChakraProvider>
 );
