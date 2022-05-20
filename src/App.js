9526cb436b5db16ed58a8dcfa01f5da6be8c78a6
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 15:40:22 -0700

    add basic error message if pokemon not found

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HStack, Input, Button, VStack, Box, Image, Badge } from '@chakra-ui/react';
+import { Text, HStack, Input, Button, VStack, Box, Image, Badge } from '@chakra-ui/react';
 import axios from 'axios';
 import PokeCard from './components/PokeCard';
 
@@ -7,14 +7,28 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
   const [searchValue, setSearchValue] = useState('');
+  const [error, setError] = useState(false);
 
   const getData = function(search) {
     setLoading(true);
+    if (search) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`).then(res => {
+      console.log(res.data);
+      setData(res.data);
+      setLoading(false);
+    })
+    .catch(function (error) {
+      console.log('logging error', error)
+      setLoading(false);
+      setError(true);
+    })
+  }
+    else
     axios.get(`https://pokeapi.co/api/v2/pokemon/zapdos`).then(res => {
       console.log(res.data);
       setData(res.data);
       setLoading(false);
-    });
+    })
   };
 
   useEffect(() => {
@@ -24,12 +38,13 @@ function App() {
 
   if (loading) return <p>Loading...</p>;
   if (!data) return <p>No data</p>;
+  if(error) return <p>Not found in Pokedex, please refresh and try again</p>
 
   return (
     <VStack pt='2em'>
       <HStack w='md'>
-       <Input ></Input>
-        <Button>Search</Button>
+       <Input onChange={e=>setSearchValue(e.target.value)} placeholder='Search Pokedex'></Input>
+        <Button onClick={()=>{getData(searchValue)}}>Search</Button>
       </HStack>
       <PokeCard data={data}></PokeCard>
     </VStack>
