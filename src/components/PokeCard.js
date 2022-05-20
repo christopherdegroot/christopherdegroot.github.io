d54425078b8fc0a5d113574dc020715cef2b7a4b
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 15:15:43 -0700

    refactor parsedStats to include visual representation for base stats

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -9,9 +9,23 @@ const parsedAbilities = props.data.abilities.map((ability)=>{
   return <p>{ability.ability.name}</p>
 })
 
-const parsedStats = props.data.stats.map((stat)=>{
-  return <p>{stat.stat.name}, {stat.base_stat}</p>
-})
+const parsedStats = props.data.stats.map(stat => {
+  return (
+    <Box key={stat.stat.name}>
+      <HStack>
+    <Text>
+      {stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)} 
+    </Text>
+    <Text fontWeight={'medium'}>{stat.base_stat}</Text>
+    </HStack>
+    <Slider defaultValue={stat.base_stat}>
+    <SliderTrack>
+      <SliderFilledTrack />
+    </SliderTrack>
+  </Slider>
+  </Box>
+  );
+});
 
 const parsedTypes = props.data.types.map((type)=>{
   let typeColors = {"normal": "#a7a778", "fire": "#f08030", "water": "#6890f0", "grass": "#78c850", "electric": "#f8d030", "ice": "#98d8d8", "fighting": "#872e2d", "poison": "#a040a0", "ground": "#e0c068", "flying": "#a890f0", "psychic": "#f75887", "bug": "#a8b820", "rock": "#b8a038", "ghost": "#705898", "dark": "#705848", "dragon": "#7038f8", "steel": "#b8b8d0",  "fairy": "#f0b6bc"}
