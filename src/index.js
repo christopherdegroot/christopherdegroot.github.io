2b90d8b097cc24dff5f5c89e3d82ed3639d664bf
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-05-20 17:10:07 -0700

    add comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// renders root with chakra provider wrapper
 root.render(
   <ChakraProvider>
     <App />
